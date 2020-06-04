160973375ce5542c5056323fccb3040d53a4c359
Steve Yadlowsky <syadlows@stanford.edu>
2020-06-04 12:48:28 -0700

    Per county trigger for CA

--- hosp_bed_demand_projection.py
+++ hosp_bed_demand_projection.py
@@ -1,5 +1,6 @@
 import numpy as np
 import warnings
+import scipy.stats
 
 from utils import GrowthRateConversion, LoSFilter
 
@@ -88,21 +89,25 @@ class HospBedDemandProjection(object):
 
     def _estimate_max_capacity_multiplier(self):
         _, new_icu_intensity_at_day, lockdown_point = self._simulate()
-        print(new_icu_intensity_at_day)
         icu_demand = self.los_filter.apply_filter(new_icu_intensity_at_day)
-        print(np.argmax(icu_demand) - lockdown_point)
         return np.max(icu_demand) / np.mean(new_icu_intensity_at_day[lockdown_point - 3:lockdown_point])
 
     def estimate_runway_threshold(self, capacity_max, z_alpha = 1.64, days_to_avg = 3.0):
-        capacity = 0.5 * (2*capacity_max + z_alpha**2 - np.sqrt(4*capacity_max*(z_alpha**2) + z_alpha**4))
+        if capacity_max > 20:
+            capacity = 0.5 * (2*capacity_max + z_alpha**2 - np.sqrt(4*capacity_max*(z_alpha**2) + z_alpha**4))
+        else:
+            for intensity in np.linspace(0.1, capacity_max, 40)[::-1]:
+                if scipy.stats.poisson.ppf(0.95, intensity) <= capacity_max:
+                    capacity = intensity
+                    break
         if self.icu:
             icu_adm_rate = float(capacity) / self._estimate_max_capacity_multiplier()
             hosp_adm_rate = icu_adm_rate / self.icu_hospitalization_fraction
         else:
             hosp_adm_rate = float(capacity) / self._estimate_max_capacity_multiplier()
             icu_adm_rate = hosp_adm_rate * self.icu_hospitalization_fraction
-        icu_adm_threshold = icu_adm_rate - z_alpha * np.sqrt(icu_adm_rate / days_to_avg)
-        hosp_adm_threshold = hosp_adm_rate - z_alpha * np.sqrt(hosp_adm_rate / days_to_avg)
+        icu_adm_threshold = scipy.stats.poisson.ppf(0.95, icu_adm_rate * days_to_avg) / days_to_avg
+        hosp_adm_threshold = scipy.stats.poisson.ppf(0.95, hosp_adm_rate * days_to_avg) / days_to_avg
         return {
             'icu_admissions_rate' : icu_adm_rate,
             'hospital_admissions_rate' : hosp_adm_rate,
